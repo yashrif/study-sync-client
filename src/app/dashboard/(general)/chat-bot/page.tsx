cc1feeebeb52c7c21e5acbc19fffefb7bcf29658
Yashrif Arifin <yaswoccho@gmail.com>
2024-09-14 13:30:15 +0600

    fix: input overlay

--- src/app/dashboard/(general)/chat-bot/page.tsx
+++ src/app/dashboard/(general)/chat-bot/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import _ from "lodash";
 import {
   Dispatch,
   SetStateAction,
   useCallback,
   useEffect,
   useMemo,
+  useRef,
   useState,
 } from "react";
 
@@ -15,7 +17,6 @@ import {
   Commands as ECommands,
   commandsLvl1,
 } from "@/assets/data/dashboard/chatBot";
-import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
@@ -24,9 +25,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select-custom";
+import { Textarea } from "@/components/ui/textarea";
 import { useFetchDataState } from "@/hooks/fetchData";
 import { UploadShallow } from "@/types";
-import _ from "lodash";
 
 const ChatBotInput = () => {
   const [text, setText] = useState("");
@@ -34,6 +35,8 @@ const ChatBotInput = () => {
   const [isSecondaryCommandsOpen, setIsSecondaryCommandsOpen] = useState(false);
   const [selectedCommand, setSelectedCommand] =
     useState<keyof typeof ECommands>();
+  const textDivRef = useRef<HTMLDivElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const filteredCommands = useMemo(
     () =>
@@ -53,17 +56,58 @@ const ChatBotInput = () => {
     if (selectedCommand) setIsSecondaryCommandsOpen(true);
   }, [selectedCommand]);
 
+  /* ------------------------------ Input overlay ----------------------------- */
+
+  useEffect(() => {
+    if (textDivRef.current) {
+      textDivRef.current.scrollTop = textDivRef.current.scrollHeight;
+      textDivRef.current.scrollLeft = textDivRef.current.scrollWidth;
+    }
+  }, [text]);
+
+  useEffect(() => {
+    if (textareaRef.current && textDivRef.current) {
+      textareaRef.current.scrollTop = textareaRef.current.scrollHeight;
+      textDivRef.current.scrollTop = textDivRef.current.scrollHeight;
+    }
+  }, [text]);
+
+  const handleScroll = () => {
+    if (textDivRef.current && textareaRef.current) {
+      textDivRef.current.scrollTop = textareaRef.current.scrollTop;
+    }
+  };
+
   return (
-    <div className="m-40 relative">
-      <Input
+    <div className="m-40 max-w-lg overflow-hidden relative">
+      <div
+        ref={textDivRef}
+        className="absolute top-0 left-0 p-2 leading-[150%] w-full h-full text-sm overflow-hidden pointer-events-none break-words whitespace-pre-wrap"
+      >
+        {text.split(" ").map((word, index) => (
+          <span
+            key={index}
+            className={index % 2 === 0 ? "text-red-500" : "text-blue-500"}
+          >
+            {word}{" "}
+          </span>
+        ))}
+      </div>
+
+      {/* Textarea */}
+      <Textarea
+        ref={textareaRef}
         value={text}
         onChange={(e) => {
           setText(e.target.value);
-          if (e.target.value === "/") setIsMainCommandsOpen(true);
+          if (e.target.value === "/") {
+            setIsMainCommandsOpen(true);
+            setSelectedCommand(undefined);
+          }
         }}
-        className="max-w-lg"
+        onScroll={handleScroll}
+        className="w-full h-full text-sm caret-primary text-transparent bg-transparent relative no-scrollbar z-10 p-2 leading-[150%] whitespace-pre-wrap"
       />
-
       <SelectContainer
         isOpen={isMainCommandsOpen}
         setIsOpen={setIsMainCommandsOpen}
@@ -118,7 +162,7 @@ const SelectContainer: React.FC<SelectContainerProps> = ({
           prev +
           (data.type === "commands"
             ? _.find(data.data, ["value", e])?.label.slice(1)
-            : _.find(data.data, ["name", e])?.title) +
+            : "using the books 📕 " + _.find(data.data, ["name", e])?.title) +
           " "
       );
     }}
