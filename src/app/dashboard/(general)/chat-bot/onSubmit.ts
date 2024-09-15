857343bf7773c14502e793842f5c46e0255d8552
Yashrif Arifin <yaswoccho@gmail.com>
2024-09-15 15:11:52 +0600

    feat: add flashcard create request

--- src/app/dashboard/(general)/chat-bot/onSubmit.ts
+++ src/app/dashboard/(general)/chat-bot/onSubmit.ts
@@ -6,7 +6,7 @@ import { useMemo } from "react";
 import { Commands } from "@/assets/data/dashboard/chatBot";
 import { links } from "@/assets/data/routes";
 import { useChatBotContext } from "@/hooks/ChatBotContext";
-import { QuizRequestDb, QuizTypes } from "@/types";
+import { QuizRequestDb, QuizTypes, Status } from "@/types";
 import { useHandlers } from "./useHandlers";
 
 type Props = {
@@ -17,7 +17,12 @@ export const useOnSubmit = () => {
   const { push } = useRouter();
 
   const { state } = useChatBotContext();
-  const { quizServerRequestHandler, quizDbRequestHandler } = useHandlers();
+  const {
+    quizServerRequestHandler,
+    quizDbRequestHandler,
+    flashcardServerRequestHandler,
+    flashcardDbRequestHandler,
+  } = useHandlers();
 
   const filteredUploads = useMemo(
     () =>
@@ -27,6 +32,8 @@ export const useOnSubmit = () => {
 
   const onSubmit = async ({ text }: Props) => {
     switch (true) {
+      /* ---------------------------------- quiz ---------------------------------- */
+
       case text.toLowerCase().includes(Commands["create-quiz"].toLowerCase()):
         if (state.selectedUploads.length > 0) {
           try {
@@ -59,7 +66,6 @@ export const useOnSubmit = () => {
             console.log(err);
           }
         }
-
         break;
       case text
         .toLowerCase()
@@ -69,10 +75,46 @@ export const useOnSubmit = () => {
       case text.toLowerCase().includes(Commands["create-slide"].toLowerCase()):
         console.log("Create Slide");
         break;
+
+      /* -------------------------------- flashcard ------------------------------- */
+
       case text
         .toLowerCase()
         .includes(Commands["create-flashcard"].toLowerCase()):
-        console.log("Create Flashcard");
+        if (state.selectedUploads.length > 0) {
+          try {
+            const serverResponse = await flashcardServerRequestHandler({
+              data: {
+                ids: state.selectedUploads,
+                types: [QuizTypes.CQ, QuizTypes.MCQ],
+              },
+              fetchType: "lazy",
+              isReset: true,
+            });
+
+            if (serverResponse) {
+              const cqs = serverResponse.cqs.map((cq) => ({
+                ...cq,
+                isFlashcard: true,
+              }));
+
+              Promise.all(
+                cqs.map(async (cq) => {
+                  await flashcardDbRequestHandler({
+                    data: cq,
+                    fetchType: "lazy",
+                    isReset: true,
+                  });
+                })
+              );
+
+              if (state.requestStatus === Status.SUCCESS)
+                push(links.dashboard.flashcard.review.href);
+            }
+          } catch (err) {
+            console.log(err);
+          }
+        }
         break;
       default:
         console.log("No command found");
--- src/app/dashboard/(general)/chat-bot/useHandlers.ts
+++ src/app/dashboard/(general)/chat-bot/useHandlers.ts
@@ -8,6 +8,8 @@ import { useFetchState } from "@/hooks/fetchData";
 import { useApiHandler } from "@/hooks/useApiHandler";
 import {
   ChatBotActionType,
+  Cq,
+  CqRequest,
   Quiz,
   QuizRequestDb,
   QuizRequestServer,
@@ -44,33 +46,70 @@ export const useHandlers = () => {
     dispatch: quizDbRequestDispatch,
   });
 
+  /* -------------------------------- Flashcard ------------------------------- */
+
+  const {
+    state: flashcardServerRequestState,
+    dispatch: flashcardServerRequestDispatch,
+  } = useFetchState<QuizResponseServer>();
+  const { handler: flashcardServerRequestHandler } = useApiHandler<
+    QuizRequestServer,
+    QuizResponseServer
+  >({
+    apiCall: useCallback(
+      (data) => studySyncServer.post(serverEndpoints.quizzes, data),
+      []
+    ),
+    dispatch: flashcardServerRequestDispatch,
+  });
+
+  const {
+    state: flashcardDbRequestState,
+    dispatch: flashcardDbRequestDispatch,
+  } = useFetchState<Cq>();
+  const { handler: flashcardDbRequestHandler } = useApiHandler<CqRequest, Cq>({
+    apiCall: useCallback(
+      (data) => studySyncDB.post(dbEndpoints.cqs, data),
+      []
+    ),
+    dispatch: flashcardDbRequestDispatch,
+  });
+
   /* --------------------------------- Status --------------------------------- */
 
   useEffect(() => {
     switch (true) {
       case quizServerRequestState.status === Status.IDLE &&
-        quizDbRequestState.status === Status.IDLE:
+        quizDbRequestState.status === Status.IDLE &&
+        flashcardServerRequestState.status === Status.IDLE &&
+        flashcardDbRequestState.status === Status.IDLE:
         dispatch({
           type: ChatBotActionType.SET_REQUEST_STATUS,
           payload: Status.IDLE,
         });
         break;
       case quizServerRequestState.status === Status.PENDING ||
-        quizDbRequestState.status === Status.PENDING:
+        quizDbRequestState.status === Status.PENDING ||
+        flashcardServerRequestState.status === Status.PENDING ||
+        flashcardDbRequestState.status === Status.PENDING:
         dispatch({
           type: ChatBotActionType.SET_REQUEST_STATUS,
           payload: Status.PENDING,
         });
         break;
-      case quizServerRequestState.status === Status.SUCCESS &&
-        quizDbRequestState.status === Status.SUCCESS:
+      case (quizServerRequestState.status === Status.SUCCESS &&
+        quizDbRequestState.status === Status.SUCCESS) ||
+        (flashcardServerRequestState.status === Status.SUCCESS &&
+          flashcardDbRequestState.status === Status.SUCCESS):
         dispatch({
           type: ChatBotActionType.SET_REQUEST_STATUS,
           payload: Status.SUCCESS,
         });
         break;
       case quizServerRequestState.status === Status.ERROR ||
-        quizDbRequestState.status === Status.ERROR:
+        quizDbRequestState.status === Status.ERROR ||
+        flashcardServerRequestState.status === Status.ERROR ||
+        flashcardDbRequestState.status === Status.ERROR:
         dispatch({
           type: ChatBotActionType.SET_REQUEST_STATUS,
           payload: Status.ERROR,
@@ -82,12 +121,23 @@ export const useHandlers = () => {
           payload: Status.IDLE,
         });
     }
-  }, [quizServerRequestState.status, quizDbRequestState.status, dispatch]);
+  }, [
+    quizServerRequestState.status,
+    quizDbRequestState.status,
+    dispatch,
+    flashcardServerRequestState.status,
+    flashcardDbRequestState.status,
+  ]);
 
   return {
     quizServerRequestHandler,
     quizServerRequestState,
     quizDbRequestHandler,
     quizDbRequestState,
+
+    flashcardServerRequestHandler,
+    flashcardServerRequestState,
+    flashcardDbRequestHandler,
+    flashcardDbRequestState,
   };
 };
