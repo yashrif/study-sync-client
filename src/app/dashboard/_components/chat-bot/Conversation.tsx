58e6b7309d4dee69a2b9aad09cc5619a4a346a6d
Yashrif Arifin <yaswoccho@gmail.com>
2024-09-22 02:32:43 +0600

    feat: add clear all button for topic & uploads in chatbot

--- src/app/dashboard/_components/chat-bot/Conversation.tsx
+++ src/app/dashboard/_components/chat-bot/Conversation.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { IconMessage } from "@tabler/icons-react";
+import { IconClearAll, IconMessage } from "@tabler/icons-react";
 import _ from "lodash";
 import { useMemo } from "react";
 
 import { ScrollArea } from "@/components/ui/scroll-area-custom";
 import { useChatBotContext } from "@/hooks/useChatBotContext";
+import { ChatBotActionType } from "@/types";
 import TopicBadge from "./_components/TopicBadge";
 import UploadBadge from "./_components/UploadBadge";
 
 const Conversation: React.FC = () => {
   const {
     state: { conversation, uploads, selectedUploads, selectedTopics, topics },
+    dispatch,
   } = useChatBotContext();
 
   const filteredUploads = useMemo(
@@ -65,13 +67,33 @@ const Conversation: React.FC = () => {
           )}
         </div>
         {(filteredUploads.length > 0 || filteredTopics.length > 0) && (
-          <div className="sticky bottom-0 inset-0 pt-2 bg-inherit flex flex-wrap-reverse gap-x-2 gap-y-1.5">
-            {filteredUploads.map((upload) => (
-              <UploadBadge key={upload.id} upload={upload} />
-            ))}
-            {filteredTopics.map((topic) => (
-              <TopicBadge key={topic.id} topic={topic} />
-            ))}
+          <div className="sticky bottom-0 inset-0 bg-accent/15 backdrop-blur">
+            <div className="relative max-h-32 overflow-y-scroll overflow-x-auto no-scrollbar flex flex-wrap-reverse gap-x-2 gap-y-1.5">
+              {filteredUploads.map((upload) => (
+                <UploadBadge key={upload.id} upload={upload} />
+              ))}
+              {filteredTopics.map((topic) => (
+                <TopicBadge key={topic.id} topic={topic} />
+              ))}
+
+              <div className="fixed w-full top-0 right-0 flex items-center justify-end z-50">
+                <div
+                  className="size-5 rounded-full ring-1 ring-destructive flex items-center justify-center cursor-pointer hover:scale-105 transition-all duration-300"
+                  onClick={() => {
+                    dispatch({
+                      type: ChatBotActionType.SET_SELECTED_TOPICS,
+                      payload: [],
+                    });
+                    dispatch({
+                      type: ChatBotActionType.SET_SELECTED_UPLOADS,
+                      payload: [],
+                    });
+                  }}
+                >
+                  <IconClearAll className="size-[14px] text-destructive" />
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </div>
