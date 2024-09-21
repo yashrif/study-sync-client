0189b9d61a9c133f402f4fda36dfc0f721e09a5e
Yashrif Arifin <yaswoccho@gmail.com>
2024-09-21 23:48:04 +0600

    fix: inlineCommands not highlight

--- src/app/dashboard/_components/chat-bot/command-items/useCommands.ts
+++ src/app/dashboard/_components/chat-bot/command-items/useCommands.ts
@@ -1,13 +1,17 @@
 "use client";
 
+import { useCallback } from "react";
+
 import {
   additionalCommands,
   Commands,
+  commandsLvl1,
   commandsLvl2,
 } from "@/assets/data/dashboard/chatBot";
 import { routes } from "@/assets/data/routes";
 import { useChatBotContext } from "@/hooks/useChatBotContext";
 import { usePath } from "@/hooks/usePath";
+import { findFirstSubstring } from "@/utils/string";
 
 const useCommands = () => {
   const { path } = usePath();
@@ -15,7 +19,17 @@ const useCommands = () => {
     state: { selectedUploads, prompt },
   } = useChatBotContext();
 
-  const commandsLvlInline = (): string[] => {
+  const getFirstLvl1Command = useCallback(() => {
+    const subString =
+      findFirstSubstring(
+        prompt.toLowerCase(),
+        commandsLvl1.map((item) => item.value.toLowerCase())
+      ).substring || "";
+
+    return prompt.trim().toLowerCase().startsWith(subString) ? subString : "";
+  }, [prompt]);
+
+  const commandsLvlInline = useCallback((): string[] => {
     const commonCommands = commandsLvl2.map((item) => item.label);
 
     switch (true) {
@@ -23,22 +37,32 @@ const useCommands = () => {
         selectedUploads.length > 0:
         return [
           ...commonCommands,
-          ...additionalCommands.slide.map((item) => item.label),
-        ];
-      case prompt
-        .toLowerCase()
-        .includes(Commands["create-slide"].toLowerCase()):
-        return [
-          ...commonCommands,
           ...additionalCommands.study.map((item) => item.label),
+          ...(getFirstLvl1Command().localeCompare(
+            Commands["create-slide"].toLowerCase()
+          ) === 0
+            ? additionalCommands.slide.map((item) => item.label)
+            : []),
         ];
       default:
         return commonCommands;
     }
-  };
+  }, [getFirstLvl1Command, path, selectedUploads.length]);
+
+  const getFirstInlineCommand = useCallback(
+    () =>
+      findFirstSubstring(
+        prompt.toLowerCase(),
+        commandsLvlInline().map((item) => item.toLowerCase())
+      ).substring || "",
+
+    [commandsLvlInline, prompt]
+  );
 
   return {
+    getFirstLvl1Command,
     commandsLvlInline,
+    getFirstInlineCommand,
   };
 };
 
